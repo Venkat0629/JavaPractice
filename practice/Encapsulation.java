22fddf122294281172d310f4db8bbe93cbdbd2ac
vveera <vveera@nisum.com>
2024-07-09 08:08:29 +0530

    Added Encapsulation code

---
+++ practice/Encapsulation.java
@@ -0,0 +1,42 @@
+package practice;
+
+class User {
+    private int id;
+    private String name;
+    private String gender;
+
+    public void setId(int id) throws Exception {
+        if (id > 0 && id < 1000) this.id = id;
+        else throw new Exception("Invalid User ID");
+    }
+
+    public void setName(String name) throws Exception {
+        if (name != null && !name.isEmpty()) this.name = name;
+        else throw new Exception("Invalid User Name");
+
+    }
+
+    public void setGender(String gender) throws Exception {
+        if (gender != null && !gender.isEmpty() || gender.equalsIgnoreCase("Male") || gender.equalsIgnoreCase("Male") || gender.equalsIgnoreCase("Other"))
+            this.gender = gender;
+        else throw new Exception("Provided Gender type is Invalid");
+
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", name='" + name + '\'' + ", gender='" + gender + '\'' + '}';
+    }
+}
+
+public class Encapsulation {
+    public static void main(String[] args) throws Exception {
+        User user = new User();
+        user.setId(100);
+        user.setGender("");
+        user.setName("");
+        System.out.println(user);
+    }
+
+
+}
